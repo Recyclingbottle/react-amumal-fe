896f8714d24e646ef8421c68a4a215f83695e12a
Recyclingbottle <hoholeo382@gmail.com>
2024-05-09 18:38:52 +0900

    App.js 각 페이지 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import PostsListPage from "./pages/PostsListPage";
+import PostDetailPage from "./pages/PostDetailPage";
+import PostCreatePage from "./pages/PostCreatePage";
+import PostEditPage from "./pages/PostEditPage";
+import UserProfileEditPage from "./pages/UserProfileEditPage";
+import PasswordChangePage from "./pages/PasswordChangePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>초기화</p>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PostsListPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/create-post" element={<PostCreatePage />} />
+          <Route path="/edit-post/:id" element={<PostEditPage />} />
+          <Route path="/edit-profile" element={<UserProfileEditPage />} />
+          <Route path="/change-password" element={<PasswordChangePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
