160bebce03262abe6e1064ab8342c9c2e871ea01
Recyclingbottle <hoholeo382@gmail.com>
2024-05-11 23:31:59 +0900

    게시글 카드 컴포넌트, 게시글 목록 페이지 사용

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,26 +1,41 @@
 import React from "react";
+import styles from "./PostCard.module.css"; // CSS 모듈 임포트
+import df_profile_img from "../assets/images/profile_img.webp";
+import { useNavigate } from "react-router-dom";
+function formatCount(count) {
+  if (count >= 1000) {
+    return `${(count / 1000).toFixed(0)}k`;
+  }
+  return count.toString();
+}
 
-function PostCard({ post, navigate, formatCount }) {
+function PostCard({ post }) {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/posts/${post.id}`);
+  };
+
   return (
-    <div className="post" onClick={() => navigate(`/posts/${post.id}`)}>
-      <h3 className="post-title">{post.title}</h3>
-      <div className="post-meta">
-        <span className="likes">좋아요 {formatCount(post.likes)}</span>
-        <span className="comments">
+    <div className={styles.postCard} onClick={handleNavigate}>
+      <h3 className={styles.postTitle}>{post.title}</h3>
+      <div className={styles.postMeta}>
+        <span className={styles.likes}>좋아요 {formatCount(post.likes)}</span>
+        <span className={styles.comments}>
           댓글수 {formatCount(post.commentsCount)}
         </span>
-        <span className="views">조회수 {formatCount(post.views)}</span>
-        <time className="post-date">
+        <span className={styles.views}>조회수 {formatCount(post.views)}</span>
+        <time className={styles.postDate}>
           {new Date(post.date).toLocaleString()}
         </time>
       </div>
-      <div className="author-info">
+      <div className={styles.authorInfo}>
         <img
-          src={post.author.profileImage_path}
+          src={post.author.profileImagePath || df_profile_img}
           alt={`${post.author.nickname}의 프로필 이미지`}
-          className="author-profile-picture"
+          className={styles.authorProfilePicture}
         />
-        <span className="author-name">{post.author.nickname}</span>
+        <span className={styles.authorName}>{post.author.nickname}</span>
       </div>
     </div>
   );
