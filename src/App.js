7fa9607204fc53ae1b65d86cfe4c8113818ddabf
Recyclingbottle <hoholeo382@gmail.com>
2024-05-11 23:30:55 +0900

    route Params 를 위한 postId 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import PostsListPage from "./pages/PostsListPage";
@@ -13,13 +13,41 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">게시글 목록 조회 페이지</Link>
+            </li>
+            <li>
+              <Link to="/posts/1">게시글 상세 조회 페이지</Link>
+            </li>
+            <li>
+              <Link to="/edit-post/1">게시글 수정 페이지</Link>
+            </li>
+            <li>
+              <Link to="/login">로그인 페이지</Link>
+            </li>
+            <li>
+              <Link to="/signup">회원가입 페이지</Link>
+            </li>
+            <li>
+              <Link to="/create-post">게시글 생성 페이지</Link>
+            </li>
+            <li>
+              <Link to="/edit-profile">회원정보수정 페이지</Link>
+            </li>
+            <li>
+              <Link to="/change-password">비밀번호 수정 페이지</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/" element={<PostsListPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/posts/:postId" element={<PostDetailPage />} />
           <Route path="/create-post" element={<PostCreatePage />} />
-          <Route path="/edit-post/:id" element={<PostEditPage />} />
+          <Route path="/edit-post/:postId" element={<PostEditPage />} />
           <Route path="/edit-profile" element={<UserProfileEditPage />} />
           <Route path="/change-password" element={<PasswordChangePage />} />
         </Routes>
