c4827ffa16df94cbb079014fbaf8353948ed0688
Recyclingbottle <hoholeo382@gmail.com>
2024-05-11 23:33:59 +0900

    게시글 작성 페이지 레이아웃, css 모듈

--- src/pages/PostCreatePage.js
+++ src/pages/PostCreatePage.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import PostForm from "../forms/PostForm";
+import styles from "../style/PostCreatePage.module.css";
 
-function PostCreatePage() {
-  return <div>게시글 작성 페이지</div>;
-}
+const PostCreatePage = () => {
+  const handleSubmit = (title, content, image) => {
+    console.log({ title, content, image });
+  };
+
+  return (
+    <>
+      <Navbar showBackButton={true} showProfileImage={true} />
+      <div className={styles.pageBody}>
+        <h3 className={styles.postCreationTitle}>게시글 작성</h3>
+        <PostForm onSubmit={handleSubmit} buttonText="완료" />
+      </div>
+    </>
+  );
+};
 
 export default PostCreatePage;
