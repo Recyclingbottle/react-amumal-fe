c8a9de236e9550a1684113136a0b92502fc2a5b0
Recyclingbottle <hoholeo382@gmail.com>
2024-05-11 23:33:33 +0900

    로그인 페이지 레이아웃, css 모듈

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,45 +1,16 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import styles from "../style/LoginPage.module.css";
 import Navbar from "../components/Navbar";
-import LoginForm from "../forms/LoginForm"; // LoginForm 컴포넌트를 임포트합니다.
-import "../style/LoginPage.css";
-
+import LoginForm from "../forms/LoginForm";
 function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [helperText, setHelperText] = useState("");
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    //console.log("로그인", email, password);
-  };
-
-  const navigateToSignup = () => {
-    navigate("/signup");
-  };
-
   return (
     <>
-      <Navbar showBackButton={false} showProfile={false} />
-      <div className="page-container">
-        <div className="login-container">
-          <p className="page-header">로그인</p>
-          <LoginForm
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            helperText={helperText}
-            setHelperText={setHelperText}
-            handleLogin={handleLogin}
-          />
-          <button
-            id="signup-button"
-            className="signup-link"
-            onClick={navigateToSignup}
-          >
-            회원가입
-          </button>
+      <Navbar showBackButton={false} ShowProfileImage={false} />
+      <div className={styles.pageContainer}>
+        <div className={styles.loginContainer}>
+          <p className={styles.pageHeader}>로그인</p>
+          <LoginForm></LoginForm>
+          <p className={styles.signupLink}>회원가입</p>
         </div>
       </div>
     </>
